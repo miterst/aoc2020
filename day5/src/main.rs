063563811cbf0678c2b7f37fa37b7012fe9d8770
Mite Ristovski <mite.ristovski@hotmail.com>
2020-12-05 19:55:19 +0100

    Improve day5 soluition

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,48 +4,21 @@ fn main() {
     let seat_ids: HashSet<i32> = include_str!("input")
         .lines()
         .map(|line| {
-            let row = line[..7]
+            let bit_string: Vec<&str> = line
                 .chars()
-                .fold((0, 127), |mut acc, next| {
-                    let diff = (acc.1 - acc.0 + 1) / 2;
-                    match next {
-                        'F' => acc.1 = acc.1 - diff,
-                        'B' => acc.0 = acc.0 + diff,
-                        _ => unreachable!(),
-                    }
-                    acc
-                })
-                .0;
+                .map(|c| if c == 'F' || c == 'L' { "0" } else { "1" })
+                .collect();
 
-            let column = line[7..]
-                .chars()
-                .fold((0, 7), |mut acc, next| {
-                    let diff = (acc.1 - acc.0 + 1) / 2;
-                    match next {
-                        'L' => acc.1 = acc.1 - diff,
-                        'R' => acc.0 = acc.0 + diff,
-                        _ => unreachable!(),
-                    }
-                    acc
-                })
-                .0;
-
-            row * 8 + column
+            i32::from_str_radix(&bit_string.join(""), 2).unwrap()
         })
         .collect();
 
-    let all_possible_seat_ids: HashSet<i32> =
-        (0..128).flat_map(|row| {
-            (0..8).map(move |column| {
-                row*8 + &column
-            })
-        }).collect();
+    let all_possible_seat_ids: HashSet<i32> = (0..1024).collect();
 
-    let missing_seat: HashSet<_> =
-        all_possible_seat_ids
-            .difference(&seat_ids)
-            .filter(|&&seat_id| seat_ids.contains(&(seat_id + 1)) && seat_ids.contains(&(seat_id - 1)))
-            .collect();
+    let missing_seat: HashSet<_> = all_possible_seat_ids
+        .difference(&seat_ids)
+        .filter(|&&seat_id| seat_ids.contains(&(seat_id + 1)) && seat_ids.contains(&(seat_id - 1)))
+        .collect();
 
     println!("{}", seat_ids.iter().max().unwrap());
     println!("{:?}", missing_seat);
